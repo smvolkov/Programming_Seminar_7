41253632f290ac7ad84fbbe93c75be8c8e130a4a
Semion Volkov <semen_volkov@mail.ru>
2023-01-05 15:06:59 +0500

    Task2 added

--- Seminar7.cs
+++ Seminar7.cs
@@ -29,7 +29,50 @@ void Matrix()
         Console.WriteLine();
     }
 
-    //Console.WriteLine(String.Join(" ", matrix));
 }
 
-Matrix();
+//Matrix();
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+int[,] IntMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-9, 10);
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    return matrix;
+}
+
+void Position(int[,] matr)
+{
+    Console.WriteLine("Введите строку: ");
+    int str = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите столбец: ");
+    int col = int.Parse(Console.ReadLine());
+
+    if (str-1 < 0 || col-1 < 0) Console.WriteLine("Неверные координаты");
+    else if (str > matr.GetLength(0) || col > matr.GetLength(1)) Console.WriteLine("Элемента с такими координатами в массиве нет");
+    else Console.WriteLine(matr[str-1, col-1]);
+}
+
+Position(IntMatrix(3,4));
